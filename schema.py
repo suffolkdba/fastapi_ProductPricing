2bcceea624d2af2d38726f3a3eba9a4861eec57e
Rob George <22867099+suffolkdba@users.noreply.github.com>
2024-05-09 15:49:05 +0100

    Update schema.py

--- schema.py
+++ schema.py
@@ -6,9 +6,16 @@ class ProductsUpdate(SQLModel):
     ProductPrice: float|None = 9.99
     StockQty: int|None = 0
 
+class ProductPrice(ProductsUpdate):
+    ProductPrice: float
+
 # Inherits from ProductsUpdate but adds the id field
 class Product(ProductsUpdate, table=True):
     id: int = Field(default=None, primary_key=True)
 
 class ProductsInsert(ProductsUpdate):
     id: int
+
+class ProductPriceByName(ProductsUpdate):
+    ProductName: str
+    ProductPrice: float
